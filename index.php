62f2e2a2e95d26937897db54380b3abdd4affbd0
Pranchal Rajpal <pranchalrajpal@gmail.com>
2026-03-28 13:50:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Slide Navbar</title>
+	<link rel="stylesheet" type="text/css" href=" style.css">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+</head>
+<body>
+
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true">
+
+			<div class="signup">
+				<form  action="validate_user.php" method="POST">
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="txt" placeholder="User name" required="">
+					<input type="email" name="email" placeholder="Email" required="">
+          		<input type="password" name="pswd" placeholder="Password" required="">
+					<button>Sign up</button>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" required="">
+					<input type="password" name="pswd" placeholder="Password" required="">
+					<button>Login</button>
+				</form>
+			</div>
+	</div>
+
+					
+  </html>
+  <?php
+	if(isset($_SESSION['success']) && isset($_SESSION['data'])) {
+echo "<div class='alert alert-success' style='width:70%;text-align:center;margin-left:15%;font-size:19px;height:5%;padding-top:5px;'>";
+                 echo $_SESSION['data'];
+echo"</div>";
+			}
+			if(isset($_SESSION['error']) && isset($_SESSION['data'])) {
+echo "<div class='alert alert-danger'style='width:70%;text-align:center;margin-left:15%;font-size:19px;height:6%;padding-top:5px;'>";
+
+                 echo $_SESSION['data'];
+echo"</div>";
+ 
+			}
+			unset($_SESSION['error']);
+			unset($_SESSION['data']);
+			unset($_SESSION['success']);
+ ?>
+
